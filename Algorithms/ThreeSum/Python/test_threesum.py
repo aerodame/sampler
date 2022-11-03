d772d2451d2748d65269ab9228bdc8cf88404ab0
Stephen Dame <sdame@uw.edu>
2022-11-03 16:21:41 -0700

    first attempt at tuple sort

--- Algorithms/ThreeSum/Python/test_threesum.py
+++ Algorithms/ThreeSum/Python/test_threesum.py
@@ -2,30 +2,19 @@ from three_sum import ThreeSum
 
 def test_A():
     s = ThreeSum()
-    test_array = [9, 11, 16, 17, 20, 35]
-    target_sum = 44
-    assert  s.run(test_array, target_sum) == [0, 5]
+    test_array = [-1,0,1,2,-1,-4]
+    target_sum = 0
+    assert  s.run(test_array, target_sum) == [[-1,-1,2],[-1,0,1]]
+    ## My incorrect output > [[-1,0,1],[-1,2,-1],[0,1,-1]]  
 
 def test_B():
     s = ThreeSum()
-    test_array = [9, 11, 16, 17, 20, 35]
-    target_sum = 100
-    assert  s.run(test_array, target_sum) == [None, None]
+    test_array = []
+    target_sum = 0
+    assert  s.run(test_array, target_sum) == []            
 
 def test_C():
     s = ThreeSum()
-    test_array = [0, 1]
-    target_sum = 1
-    assert  s.run(test_array, target_sum) == [0, 1]        
-
-def test_D():
-    s = ThreeSum()
-    test_array = []
-    target_sum = 42
-    assert  s.run(test_array, target_sum) == [None, None]            
-
-def test_E():
-    s = ThreeSum()
-    test_array = [1, 2, 3, 4, 5]
-    target_sum = -1
-    assert  s.run(test_array, target_sum) == [None, None]
+    test_array = [-47, -46, -38, -37, -33, -16, -13, -11, -4, -2, 2, 7, 17, 26, 33, 35, 39, 43, 46, 47]
+    target_sum = 0
+    assert  s.run(test_array, target_sum) == [[-46, 7, 39], [-37, -2, 39], [-37, 2, 35], [-33, -13, 46], [-33, -2, 35], [-33, 7, 26], [-13, -4, 17]]    
--- Algorithms/ThreeSum/Python/three_sum.py
+++ Algorithms/ThreeSum/Python/three_sum.py
@@ -5,25 +5,38 @@ such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
 Notice that the solution set must not contain duplicate triplets.
 """
 class ThreeSum:
+
+    ## given a list of 3 integers, sort and return a tuple
+    def __tuple_sort(self, triple: list[int]) -> tuple:
+        return (0,1,2)
+
     def run(self, nums: list[int], target: int) -> list[list[int]]:
         print(f'nums:{nums} target:{target}')
         count = 0
-        hits = []
+        x = self.__tuple_sort([2,0,1])
+        print(x)
+        hits = set()
         none_found = True
         for i in range(len(nums)-1):
             for j in range(i+1, len(nums)-1):
                 for k in range(j+1, len(nums)):
-                    sum = nums[i] + nums[j] + nums[k]
+                    xi = nums[i]
+                    xj = nums[j]
+                    xk = nums[k]
+                    sum = xi + xj + xk
                     # print(f'iter:{count} -> i:{i} j:{j} k:{k} sum:{sum}')
                     if (sum == 0):
                         none_found = False
-                        triples = [nums[i],nums[j],nums[k]]
-                        strTriples = f'({nums[i]},{nums[j]},{nums[k]})'
-                        hits.append(triples)
-                        # print(triples)    
+                        l = [xi, xj, xk]
+                        l.sort()
+                        sorted_tuples = tuple(l)
+                        strTuples = f'({xi},{xj},{xk})'
+                        hits.add(sorted_tuples)
+                        # print(tuples)    
                     count = count + 1
-        return hits
-
-        
+        ans = []
+        for i in hits:
+            ans.append(list(i))  
+        return sorted(ans)
 
 
