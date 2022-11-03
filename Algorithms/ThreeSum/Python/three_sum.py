0a6b956dc3a2b4b92531e4870ef091e628aa1896
Stephen Dame <sdame@uw.edu>
2022-11-03 09:20:48 -0700

    fix return data structure spec

--- Algorithms/ThreeSum/Python/three_sum.py
+++ Algorithms/ThreeSum/Python/three_sum.py
@@ -5,7 +5,7 @@ such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
 Notice that the solution set must not contain duplicate triplets.
 """
 class ThreeSum:
-    def run(self, nums: list[int], target: int) -> list[int]:
+    def run(self, nums: list[int], target: int) -> list[list[int]]:
         print(f'nums:{nums} target:{target}')
         count = 0
         hits = []
@@ -17,7 +17,7 @@ class ThreeSum:
                     # print(f'iter:{count} -> i:{i} j:{j} k:{k} sum:{sum}')
                     if (sum == 0):
                         none_found = False
-                        triples = (nums[i],nums[j],nums[k])
+                        triples = [nums[i],nums[j],nums[k]]
                         strTriples = f'({nums[i]},{nums[j]},{nums[k]})'
                         hits.append(triples)
                         # print(triples)    
